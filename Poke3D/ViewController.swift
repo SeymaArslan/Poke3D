118f1e72e2b6def244350d57ac6632b6435d6be0
Şeyma Arslan <seymarslannn@gmail.com>
2023-12-19 11:33:31 +0300

    Add tracking image

--- Poke3D/ViewController.swift
+++ Poke3D/ViewController.swift
@@ -21,19 +21,16 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
+        let configuration = ARImageTrackingConfiguration()
+        
+        let imageToTrack = ARReferenceImage.referenceImages(inGroupNamed: "Pokemon Cards", bundle: Bundle.main)
 
         // Run the view's session
         sceneView.session.run(configuration)
